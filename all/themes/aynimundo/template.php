d4fb23b2518cb95e137e285b12233565d60a3a49
Ralph Moser <ralph@ramosoft.ch>
2016-12-22 13:25:21 +0100

    - google fonts with https

--- all/themes/aynimundo/template.php
+++ all/themes/aynimundo/template.php
@@ -1,6 +1,6 @@
 <?php
 	function aynimundo_preprocess_page(&$variables) {
-     drupal_add_css('http://fonts.googleapis.com/css?family=Cabin+Condensed:400,500,700|Open+Sans+Condensed:700,300', array('type' => 'external', 'group' => CSS_THEME, 'weight' => -10));
+     drupal_add_css('https://fonts.googleapis.com/css?family=Cabin+Condensed:400,500,700|Open+Sans+Condensed:700,300', array('type' => 'external', 'group' => CSS_THEME, 'weight' => -10));
 	}
 	function aynimundo_links__locale_block($variables) {
 		foreach ($variables['links'] as $langcode => $link) {
